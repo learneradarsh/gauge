83c816be4efe8f925ffeb6c46699c0cf72d56049
BugDiver <vinaysh@thoughtworks.com>
2018-10-19 11:40:04 +0530

    WIP fixing builds

--- build/pip/build.py
+++ build/pip/build.py
@@ -7,14 +7,16 @@ from subprocess import call
 
 
 base_url = 'https://api.github.com/repos/getgauge/gauge/releases'
-latest_version = json.loads(requests.get(base_url).text)[0]['tag_name'].replace('v', '')
+latest_version = json.loads(requests.get(base_url).text)[
+    0]['tag_name'].replace('v', '')
+
 
 def create_setup_file():
     tmpl = open("setup.tmpl", "r")
     setup = open("setup.py", "w+")
     version = os.getenv("GAUGE_VERSION") or latest_version
     name = os.getenv("GAUGE_PACKAGE_NAME") or "getgauge-cli"
-    setup.write(tmpl.read().format(version,name))
+    setup.write(tmpl.read().format(version, name))
     setup.close()
     tmpl.close()
 
@@ -24,14 +26,21 @@ def generate_package():
     print('Creating getgauge package.')
     create_setup_file()
     print(sys.executable)
-    call([sys.executable, '-m', 'pip', '--version'])
-    call([sys.executable, '-m', 'pip', 'show', 'setuptools'])
-    call([sys.executable, 'setup.py', 'sdist'], stdout=sys.stdout, stderr=sys.stderr)
+    call([sys.executable, '-m', 'pip', '--version'],
+         stdout=sys.stdout, stderr=sys.stderr)
+    call([sys.executable, '-m', 'pip', 'show', 'setuptools'],
+         stdout=sys.stdout, stderr=sys.stderr)
+    c = call([sys.executable, 'setup.py', 'sdist'],
+             stdout=sys.stdout, stderr=sys.stderr)
+    sys.exit(c)
 
 
 def install():
     create_setup_file()
-    call([sys.executable, 'setup.py', 'install'], stdout=sys.stdout, stderr=sys.stderr)
+    c = call([sys.executable, 'setup.py', 'install'],
+             stdout=sys.stdout, stderr=sys.stderr)
+    sys.exit(c)
+
 
 usage = """
 Usage: python build.py --[option]
@@ -52,5 +61,6 @@ def main():
             generate_package()
         else:
             print(usage)
+
 
 main()
